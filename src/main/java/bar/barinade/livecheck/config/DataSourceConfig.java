bc4ac72a3377bd95c75e229f9eb1e28fb7b97117
Barinade <poco0317@users.noreply.github.com>
2021-11-28 01:48:01 -0600

    initial configuration stuff

---
+++ src/main/java/bar/barinade/livecheck/config/DataSourceConfig.java
@@ -0,0 +1,35 @@
+package bar.barinade.livecheck.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataSourceConfig {
+	
+	@Value("livecheck.db.driverclassname")
+	private String driverClassName;
+	
+	@Value("livecheck.db.url")
+	private String url;
+	
+	@Value("livecheck.db.user")
+	private String user;
+	
+	@Value("livecheck.db.password")
+	private String password;
+
+	@Bean
+	public DataSource dataSource() {
+	    final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	    dataSource.setDriverClassName(driverClassName);
+	    dataSource.setUrl(url);
+	    dataSource.setUsername(user);
+	    dataSource.setPassword(password);
+	    return dataSource;
+	}
+
+}
