140261ca164f6a6ec9e752c5f9cdc02fcb66a860
Barinade <poco0317@users.noreply.github.com>
2021-11-28 01:25:50 -0600

    init

---
+++ src/main/java/bar/barinade/livecheck/LivecheckJavaApplication.java
@@ -0,0 +1,13 @@
+package bar.barinade.livecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivecheckJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivecheckJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bar/barinade/livecheck/LivecheckJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package bar.barinade.livecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivecheckJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
