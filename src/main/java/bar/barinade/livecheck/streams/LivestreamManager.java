5c2ebc1579a05cc9f491fe97dcdbbce01dcae853
Barinade <poco0317@users.noreply.github.com>
2022-01-12 00:12:39 -0600

    wip impl for livestream aggregation

--- src/main/java/bar/barinade/livecheck/streams/LivestreamManager.java
+++ src/main/java/bar/barinade/livecheck/streams/LivestreamManager.java
@@ -1,5 +1,7 @@
 package bar.barinade.livecheck.streams;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +13,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 import bar.barinade.livecheck.discord.serverconfig.service.ServerConfigService;
+import bar.barinade.livecheck.streams.data.LivestreamInfo;
 import bar.barinade.livecheck.streams.data.repo.LivestreamInfoRepo;
 import bar.barinade.livecheck.streams.data.repo.PostedLivestreamRepo;
 import bar.barinade.livecheck.streams.twitch.TwitchLivestreamImpl;
@@ -49,6 +52,21 @@ public class LivestreamManager {
 	 * This function normally should be run on a loop, doing most of the work.
 	 */
 	public void aggregate() {
+		
+		// all streamers
+		List<LivestreamInfo> allStreamsBefore = streamRepo.findAll();
+		
+		// all streamers mapped out by platform
+		// this mapping is adjusted based on the streamers that went live or offline, to derive the livestreams to post
+		HashMap<LivestreamImpl.Type, HashMap<String, LivestreamInfo>> streamersByPlatform = new HashMap<>();
+		for (LivestreamInfo stream : allStreamsBefore) {
+			final LivestreamImpl.Type platform = stream.getId().getPlatform();
+			if (!streamersByPlatform.containsKey(platform)) {
+				streamersByPlatform.put(platform, new HashMap<>());
+			}
+			streamersByPlatform.get(platform).put(stream.getId().getName(), stream);
+		}
+		
 		
 	}
 }
